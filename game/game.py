d425715a58e5dd2aadc708f30c82cbdce8c87b5c
y70131 <yuukiyamada2005@gmail.com>
2022-10-31 15:04:40 +0900

    勝率の計算を行うメソッドを追加

--- game/game.py
+++ game/game.py
@@ -49,6 +49,62 @@ class Game:
             self.ui.display(self.board)
 
         self.ui.displayResult(self.board)
+    
+    def calculatePlayResult(self, playNum: int) -> None:
+        """対戦を何度も行い、勝率を計算する
+
+        Args:
+            playNum (int): 対戦の試行回数
+        """
+        blackWin = 0
+        whiteWin = 0
+        draw = 0
+
+        progress = 0
+        for i in range(playNum):
+            if ((i + 1) / playNum) >= progress:
+                print(f"progress... {float(progress * 100):.4}%")
+                progress += 0.05
+
+            winner, (blackDiscNum, WhiteDiscNum) = self.playWithoutDisplay()
+            if winner == Disc.black:
+                blackWin += 1
+            elif winner == Disc.white:
+                whiteWin += 1
+            else:
+                draw += 1
+        
+        print(f"progress... 100%")
+        print(f"play: {playNum} times")
+        print(f"black win: {blackWin}, white win: {whiteWin}, draw: {draw}")
+        print(f"winning percentage; black: {blackWin / playNum * 100:.3}%, white: {whiteWin / playNum * 100:.3}%")
+    
+    def playWithoutDisplay(self) -> tuple[Disc, tuple[int, int]]:
+        """表示無しで対戦する
+
+        Returns:
+            tuple[Disc, tuple[int, int]]: 勝者の石の色、黒石の数、白石の数
+        """
+        self.board = BitBoard()
+        while True:
+            if self.board.player == Disc.black:
+                selection = self.firstSolver.selectLocation(self.board)
+            elif self.board.player == Disc.white:
+                selection = self.secondSolver.selectLocation(self.board)
+            
+            if selection == "pass":
+                self.board.passPut()
+            elif selection == "undo":
+                self.board.undo()
+                continue
+            else:
+                self.board.put(selection)
+
+            if self.board.gameIsFinished():
+                break
+            self.board.updateBoardStatus()
+        
+        return (self.board.getWinner(), self.board.getDiscNum())
 
     # ソルバーのオブジェクトを生成
     def generateSolver(self, name: str) -> Solver:
